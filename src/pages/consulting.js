0fc39041b6609f1a2598e9af3479eff943276da6
Chris B <chris.bowles@me.com>
2022-03-24 14:43:28 +0000

    Some new test pages

---
+++ src/pages/consulting.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import get from 'lodash/get'
+
+import Layout from '../components/layout'
+
+function ConsultingPage(props) {
+  return (
+      <div className="about-container">
+        <p>Ventures.</p>
+      </div>
+  )
+}
+export default ConsultingPage
